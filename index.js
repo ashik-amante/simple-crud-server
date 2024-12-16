4c6a6599dad4c5786ec0ec284a07522565091ade
Ashik <abdullahashik17@gmail.com>
2024-12-17 01:39:09 +0600

    basic complete

--- index.js
+++ index.js
@@ -56,6 +56,23 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result)
     })
+    // data update put
+    app.put('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const user = req.body;
+      console.log(user);
+      const filter = {_id: new ObjectId(id)}
+      const option = {upsert: true}
+      const updatedUser = {
+        $set:{
+          name : user.name,
+          email : user.email
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedUser, option);
+      res.send(result)
+
+    })
 
     // delete
     app.delete('/users/:id', async(req,res)=>{
